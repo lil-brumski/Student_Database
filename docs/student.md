96b8a696265a53c776123e8f87af32bae3e452ca
lil-brumski <davidbrume7@gmail.com>
2024-09-18 08:58:22 +0100

    Changes...

--- docs/student.md
+++ docs/student.md
@@ -6,6 +6,6 @@
 
 * The `admissionList()` outputs the admitted profiles to an `admission-list.txt` file.
 
-* To use these methods, an object has to be instantiated or a smart pointer as shown [here](https://github.com/lil-brumski/Student_Database/blob/main/test%2Fmain.cpp).
+* To use these methods, an object has to be instantiated or a smart pointer as shown [here](https://github.com/lil-brumski/Student_Database/blob/main/test%2Ftest3.cpp).
 
-* You don't have to chain the methods or use smart pointers [like I did in the test/main.cpp file](https://github.com/lil-brumski/Student_Database/blob/main/test%2Fmain.cpp), you can separate them like in [this one](https://github.com/lil-brumski/Student_Database/blob/main/test%2Ftest2.cpp)
+* You don't have to chain the methods or use smart pointers [like I did in the test/main.cpp file](https://github.com/lil-brumski/Student_Database/blob/main/test%2Ftest3.cpp), you can separate them like in [this one](https://github.com/lil-brumski/Student_Database/blob/main/test%2Ftest2.cpp)
--- tests/main.cpp
+++ tests/main.cpp
@@ -6,11 +6,7 @@
 
 int main(){
   
-  std::ifstream NAMES{"candidate-list.txt"};
-  
-  if(!NAMES.is_open()){
-    std::cout << "Can\'t open file, try again.\n";
-  }
+  std::ifstream NAMES{"../tests/candidate-list.txt"};
   
   //For storing the names of the values gotten from the NAME object.
   std::string collector, individual_names;
@@ -23,25 +19,33 @@ int main(){
   //These values store the generated random numbers.
   int v_olevel_credits{}, v_age{}, v_jamb_score{}, v_post_utme{};
   
+  if(!NAMES.is_open()){
+    std::cout << "Can\'t open file, try again.\n";
+  }
+  else{//else-begins.
+    
+    while(true){//First While Loop.
+      auto studentInfo = std::make_unique<Database>();
+    
+      while(std::getline(NAMES, collector)){//Second While Loop.
+      
+        individual_names = collector;
+      
+        v_olevel_credits = r_olevel_credits(gen);
+        v_age = r_age(gen);
+        v_jamb_score = r_jamb_score(gen);
+        v_post_utme = r_post_utme(gen);
+    
+        studentInfo->checkStudent(individual_names, v_olevel_credits, v_age, v_jamb_score, v_post_utme).admission().admissionList();
+      
+     }//Second While Loop.    
+    
+     break;    
+    }//First While Loop.
+    
+    std::cout << "Success!\n";
+  }//else-ends.
   
-  while(true){//First While Loop.
-    auto studentInfo = std::make_unique<Database>();
-    
-    while(std::getline(NAMES, collector)){//Second While Loop.
-      
-      individual_names = collector;
-      
-      v_olevel_credits = r_olevel_credits(gen);
-      v_age = r_age(gen);
-      v_jamb_score = r_jamb_score(gen);
-      v_post_utme = r_post_utme(gen);
-    
-      studentInfo->checkStudent(individual_names, v_olevel_credits, v_age, v_jamb_score, v_post_utme).admission().admissionList();
-      
-    }//Second While Loop.    
-    
-    break;    
-  }//First While Loop.
   
-  std::cout << "Success!\n";
+  
 }
